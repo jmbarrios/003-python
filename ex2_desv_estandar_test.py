f85b8ec0548ced2b17692a4b80eb2a8701e2c9f8
Juan Martín Barrios <j.m.barrios@gmail.com>
2021-11-28 23:56:57 -0600

    FIX Cambios en pruebas

--- ex2_desv_estandar_test.py
+++ ex2_desv_estandar_test.py
@@ -4,8 +4,8 @@ import homework
 
 @pytest.mark.parametrize('test_input, expected',[
     ([1,1,1,1,1,1,1,1], 0),
-    ([1,0,1,0,1,0], 0.5477225575051661),
-    ([0.5,0.5,1,1], 0.28867513459481287)
+    ([1,0,1,0,1,0], 0.5477),
+    ([0.5,0.5,1,1], 0.2886)
 ])
 def test_desv_estandar(test_input, expected):
-    assert homework.desv_estandar(test_input)==expected
+    assert homework.desv_estandar(test_input) == pytest.approx(expected, 0.0001)
--- ex2_normalizar_test.py
+++ ex2_normalizar_test.py
@@ -3,7 +3,7 @@ import homework
 
 
 @pytest.mark.parametrize('test_input, expected',[
-    ([1,2],[-0.7071067811865475, 0.7071067811865475]),
+    ([[1,2]], [-0.707106, 0.707106]),
 ])
 def test_normalizar(test_input, expected):
-    assert homework.normalizar(test_input)==expected
+    assert homework.normalizar(test_input) == pytest.approx(expected, 0.000001)
